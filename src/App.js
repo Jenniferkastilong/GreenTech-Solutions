2bd049bc47f3edda1a834532cc98277fc6b8a494
Jennifer Kastilong <jenniferkastilong@gmail.com>
2025-01-16 08:49:51 +0700

    terbaru

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products.jsx";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
 
 const App = () => {
-  console.log("Aplikasi dimulai!");
-
   return (
-    <div>
-      <h1>Test Logging</h1>
-    </div>
+    <Router basename="/GreenTect-Solutions">
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
